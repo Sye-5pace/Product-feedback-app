40e9a7ee26931a61ff2f420b5001bc21f0aa3048
Sye-5pace <owusilasyp00@gmail.com>
2024-01-28 15:49:39 +0000

    :lipstick: Style: add fade transition from vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <main class="flex justify-center w-full min-h-screen p-0 m-0 border-box bg-[#F7F8FD] font-jost">
+  <v-app class="flex justify-center w-full min-h-screen p-0 m-0 border-box bg-[#F7F8FD] font-jost">
     <div class="my-[2.5rem] mobile:m-0 desktop:w-[69.375rem] tablet:w-[43.0625rem] tablet:mx-auto desktop:mx-[p3rem]  flex mobile:w-full">
-      <router-view ></router-view>
+      <v-fade-transition>
+        <router-view ></router-view>
+      </v-fade-transition>
     </div>
-  </main>
+  </v-app>
 </template>
 
 <script setup lang="ts">
